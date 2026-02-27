6cfe027405865dbd3b09eaa872d70aa388336706
Oleksandr <starling.oleksandr@gmail.com>
2026-02-27 22:33:05 +0000

    Change message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def home():
- return jsonify(status="ok", message="Hello from Flask CI/CD!")
+ return jsonify(status="ok", message="Hello from Flask CI/CD! by Oleksandr Shpakovsky")
 @app.route("/health")
 def health():
  return jsonify(status="healthy")
